0658e67046c466dfc17e7375598c2f08fbebab67
Sakti Dwi Cahyono <54krpl@gmail.com>
2012-08-13 08:01:20 +0700

    add videoadmin some options

--- pytube/admin.py
+++ pytube/admin.py
@@ -3,7 +3,9 @@ from .models import Video
 
 
 class VideoAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'user', 'created_time', 'view_count']
+    search_fields = ['name', 'desc']
+    date_hierarchy = 'created_time'
 
 
 admin.site.register(Video, VideoAdmin)
